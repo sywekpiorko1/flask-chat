912c8496cd156ee1cb9b05622691c1072edbdb41
Sylwester <sylwesterpiorkowski@yahoo.ie>
2019-02-26 15:08:29 +0000

    Add timestamps and index.html with form to check if cookie session exists

--- run_sp.py
+++ run_sp.py
@@ -9,39 +9,40 @@ messages = []
 
 
 def add_messages(username, message):
-    """Add messages to the `messages` list"""
+    """ Add messages to messages ist """
     now = datetime.now().strftime("%H:%M:%S")
     messages.append("({}) {}: {}".format(now, username, message))
 
 
 def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
+    """ Get all messages and separate using a 'br' tag """
     return "<br>".join(messages)
 
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route('/', methods = ["POST", "GET"])
 def index():
-    """Main page with instructions"""
+    """ Main page with instructions """
+    
     if request.method == "POST":
         session["username"] = request.form["username"]
-
+        
     if "username" in session:
         return redirect(session["username"])
+    
+    return  render_template("index.html")
 
-    return render_template("index.html")
-
-
-@app.route("/<username>")
+    
+@app.route('/<username>')
 def user(username):
-    """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    """ Display chat messages """
+    return "<h1>Welcome, {0}</h1> {1}".format(username, get_all_messages())
 
-
-@app.route("/<username>/<message>")
+    
+@app.route('/<username>/<message>')
 def send_message(username, message):
-    """Create a new message and redirect back to the chat page"""
+    """ Create new message and redirect to the chat page """
     add_messages(username, message)
     return redirect(username)
+    
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
 
-
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)

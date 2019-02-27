647ee29b93ffc65996df8769aef248a5dd8e44da
Sylwester <sylwesterpiorkowski@yahoo.ie>
2019-02-27 09:03:35 +0000

    Add chat textbox functionality and refactored code

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 
 
 app = Flask(__name__)
@@ -8,15 +8,10 @@ app.secret_key = "randomstring123"
 messages = []
 
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
-
-
-def get_all_messages():
-    """Get all of the messages and separate them with a `br`"""
-    return "<br>".join(messages)
+    messages.append({"timestamp": now, "from": username, "message": message})
 
 
 @app.route("/", methods = ["GET", "POST"])
@@ -26,22 +21,20 @@ def index():
         session["username"] = request.form["username"]
 
     if "username" in session:
-        return redirect(session["username"])
+        return redirect(url_for("user", username=session["username"]))
 
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/chat/<username>", methods = ["GET", "POST"])
 def user(username):
-    """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
-
-
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    """Create a new message and redirect back to the chat page"""
-    add_messages(username, message)
-    return redirect(username)
-
+    """Add and display chat messages"""
+    
+    if request.method == "POST":
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+        
+    return render_template("chat.html", username = username, chat_messages = messages)
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
